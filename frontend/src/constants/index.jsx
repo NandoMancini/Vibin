667baab49ca1926810d82ba64bbe8364185f66d7
Fernando Mancini de Sousa <fernandomancinidesousa@gmail.com>
2026-02-26 21:49:31 -0500

    Great Reset

--- frontend/src/constants/index.jsx
+++
@@ -1,6 +0,0 @@
-export const NAVIGATION_LINKS = [
-  {label: "About", href: "#about"},
-  {label: "Chat", href: "#chat"},
-  {label: "Translate", href: "#translate"},
-  {label: "Caption", href: "#caption"},
-];
